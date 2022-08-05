fdb421b34e6a0baf66b4fa9227d23bb3047042b7
MrOlegBar <Legan26021991@yandex.ru>
2022-08-05 17:44:09 +0300

    save to continue

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,37 +1,43 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.exception.InvalidEmailException;
 import ru.yandex.practicum.catsgram.exception.UserAlreadyExistException;
 import ru.yandex.practicum.catsgram.model.User;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    private final List<User> users = new ArrayList<>();
+    private final Set<User> users = new HashSet<>();
 
-    @GetMapping("/posts")
-    public List<User> findAll() {
+    @GetMapping
+    public Set<User> findAll() {
         return users;
     }
 
-    @PostMapping(value = "/post")
+    @PostMapping
     public void create(@RequestBody User user) {
-        if (users.contains(user)) {
-            throw new UserAlreadyExistException("Пользователь с указанным адресом электронной почты уже был добавлен ранее");
-        } else if (user.getEmail() == null || user.getEmail().equals("")) {
+        if (user.getEmail() == null || user.getEmail().equals("")) {
             throw new InvalidEmailException("В переданных данных отсутствует адрес электронной почты");
+        } else if (users.contains(user)) {
+            throw new UserAlreadyExistException("Пользователь с указанным адресом электронной почты уже был добавлен ранее");
         } else {
             users.add(user);
         }
     }
 
-    @PutMapping("/users")
+    @PutMapping
     public void update(@RequestBody User user) {
-
+        if (user.getEmail() == null || user.getEmail().equals("")) {
+            throw new InvalidEmailException("В переданных данных отсутствует адрес электронной почты");
+        } else if (users.contains(user)) {
+            users.remove(user);
+            users.add(user);
+        } else {
+            users.add(user);
+        }
     }
 }
